e525ea8a788332e1d19b81efbcdcebe83acdf080
Katerina <e.a.golovinova@gmail.com>
2020-05-26 22:12:57 +0300

    Updates

---
+++ precache-manifest.639721a4ba991da21acbf6a8563ce496.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759da0e22be1c3c2c68b177cf8c9ee5f",
+    "url": "/social-work/index.html"
+  },
+  {
+    "revision": "f894ab71fafcdbcfd5bd",
+    "url": "/social-work/static/css/3.a35dae37.chunk.css"
+  },
+  {
+    "revision": "7c6b8b70c2df7d58d81e",
+    "url": "/social-work/static/css/4.76141b5b.chunk.css"
+  },
+  {
+    "revision": "ca509dcb039acdae3f15",
+    "url": "/social-work/static/css/main.90764c4d.chunk.css"
+  },
+  {
+    "revision": "4e2a8ecd88d1c7766274",
+    "url": "/social-work/static/js/2.4cb43953.chunk.js"
+  },
+  {
+    "revision": "307ebae611fce29f55b0f8679f250f85",
+    "url": "/social-work/static/js/2.4cb43953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f894ab71fafcdbcfd5bd",
+    "url": "/social-work/static/js/3.7ad00abe.chunk.js"
+  },
+  {
+    "revision": "7c6b8b70c2df7d58d81e",
+    "url": "/social-work/static/js/4.8a453234.chunk.js"
+  },
+  {
+    "revision": "ca509dcb039acdae3f15",
+    "url": "/social-work/static/js/main.d6f6dd9d.chunk.js"
+  },
+  {
+    "revision": "2b06f9ec9d5b76c6f658",
+    "url": "/social-work/static/js/runtime-main.895eb4d4.js"
+  },
+  {
+    "revision": "5c911b6a87d5aab830e1a98769235e6f",
+    "url": "/social-work/static/media/loading.5c911b6a.svg"
+  }
+]);
